2e7133ef3f0dbab07fe6aa5ce16d9f1ebf2a4909
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2019-12-10 20:34:26 +0100

    test

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log('test');
+// fizz buzz
+
+
+const fizzbuzz = (num) => {
+  for (let i = 1; i <= num; i++) {
+    let element = i;  // create i 
+    if (i % 3 === 0 && i % 5 === 0) {
+      element = 'fizzbuzz';
+      console.log(`${i} ` + element);
+    }
+    else if (i % 3 === 0) {
+      element = 'Fizz';
+      console.log(`${i} ` + element);
+    } else if (i % 5 === 0) {
+      element = 'Buzz'
+      console.log(`${i} ` + element);
+    }
+  }
+}
+
+fizzbuzz(30);

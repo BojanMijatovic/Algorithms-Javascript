229ddaa795b779e3742d1126ec0c54d71aa6e686
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2019-12-10 20:46:03 +0100

    add

--- script.js
+++ script.js
@@ -1,21 +1,33 @@
+
+
 // fizz buzz
 
+// const fizzbuzz = (num) => {
+//   for (let i = 1; i <= num; i++) {
+//     let element = i;  // create i 
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       element = 'fizzbuzz';
+//       console.log(`${i} ` + element);
+//     }
+//     else if (i % 3 === 0) {
+//       element = 'Fizz';
+//       console.log(`${i} ` + element);
+//     } else if (i % 5 === 0) {
+//       element = 'Buzz'
+//       console.log(`${i} ` + element);
+//     }
+//   }
+// }
 
-const fizzbuzz = (num) => {
-  for (let i = 1; i <= num; i++) {
-    let element = i;  // create i 
-    if (i % 3 === 0 && i % 5 === 0) {
-      element = 'fizzbuzz';
-      console.log(`${i} ` + element);
-    }
-    else if (i % 3 === 0) {
-      element = 'Fizz';
-      console.log(`${i} ` + element);
-    } else if (i % 5 === 0) {
-      element = 'Buzz'
-      console.log(`${i} ` + element);
-    }
-  }
-}
+// fizzbuzz(30);
 
-fizzbuzz(30);
+
+
+// Harmless Ransom Note
+const log = (arr) => {
+  console.log(arr[0]);
+  console.log(arr[1]);
+};
+
+log([1, 3, 4, 5, 6]);
+log([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]);

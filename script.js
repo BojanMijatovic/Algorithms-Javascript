ed6ac05536f6e9c0b379299d7fd602a4ea236010
bojan mijatovic <bojan.freelance.jobs@gmail.com>
2019-12-13 20:20:15 +0100

    add

--- script.js
+++ script.js
@@ -24,26 +24,45 @@
 
 // New Algorithm
 // Harmless Ransom Note
-const harmlessRansomNote = (noteText, magazineText) => {
-  let noteTextArr = noteText.split(' ');
-  let magazineTextArr = magazineText.split(' ');
-  const magazineObj = {};
-  magazineTextArr.forEach(word => {
-    if (!magazineObj[word]) magazineObj[word] = 0;
-    magazineObj[word]++;
+// const harmlessRansomNote = (noteText, magazineText) => {
+//   let noteTextArr = noteText.split(' ');
+//   let magazineTextArr = magazineText.split(' ');
+//   const magazineObj = {};
+//   magazineTextArr.forEach(word => {
+//     if (!magazineObj[word]) magazineObj[word] = 0;
+//     magazineObj[word]++;
+//   });
+
+//   let noteIsPossible = true;
+//   noteTextArr.forEach(word => {
+//     if (magazineObj[word]) {
+//       magazineObj[word]--;
+//       if (magazineObj[word] < 0) noteIsPossible = false;
+//     }
+//     else noteIsPossible = false;
+//   })
+
+//   return noteIsPossible;
+// }
+
+
+// console.log(harmlessRansomNote('ber test secret', 'test s ads now  s sad ber deer sda ber beer  ber'));
+
+// new Algorithm
+//Is palindrome
+
+const isPalindrome = (str) => {
+  str = str.toLowerCase();
+  let charArr = str.split('');
+  let validCharacters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  let lettersArr = [];
+  charArr.forEach(char => {
+    if (validCharacters.indexOf(char) > -1) lettersArr.push(char);
   });
 
-  let noteIsPossible = true;
-  noteTextArr.forEach(word => {
-    if (magazineObj[word]) {
-      magazineObj[word]--;
-      if (magazineObj[word] < 0) noteIsPossible = false;
-    }
-    else noteIsPossible = false;
-  })
-
-  return noteIsPossible;
+  return lettersArr.join('') === lettersArr.reverse().join('');
 }
 
 
-console.log(harmlessRansomNote('ber test secret', 'test s ads now  s sad ber deer sda ber beer  ber'));
+console.log(isPalindrome('test one'));
